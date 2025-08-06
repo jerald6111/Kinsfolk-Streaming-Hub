45c0da81a5a97d5fda71eea20874a04ce4ce0685
Jerald Lanchinebre <jerald.lanchinebre@gmail.com>
2025-08-06 09:13:45 +0800

    Fix: Remove next export for Vercel build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
-}
+  // All the necessary deployment and optimization settings
+  // are handled automatically by Vercel.
+  // You can add other project-specific configurations here if you need them.
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
